deee09a23dec5d9be3864f17f310eebd46bed6ec
TawanABC <tawan.kunanon@gmail.com>
2025-04-06 18:04:07 +0700

    add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+
+    const token = request.cookies.get('authToken')?.value;
+
+    if (!token) {
+        const loginUrl = new URL('/login', request.url);
+        return NextResponse.redirect(loginUrl);
+    }
+
+    return NextResponse.next();
+
+}
+
+export const config = {
+    matcher: [],
+    // matcher: ['/home/:path*'],
+};
