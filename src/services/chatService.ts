17a7780e7908e1c3d1b86338f12ab9f608f7432d
Pratya Benjawan <123279951+BertPratya@users.noreply.github.com>
2025-04-10 19:29:22 +0700

    connect with API

--- src/services/chatService.ts
+++ src/services/chatService.ts
@@ -46,3 +46,177 @@ export const getUserChatRooms = async () => {
         throw error;
     }
 }
+
+export const createMessage = async ({ chatRoomId, messageType, content }: {
+    chatRoomId: string, messageType: string, content: string
+}) => {
+    try {
+        const data = {
+            chatRoomId,
+            messageType,
+            content
+        };
+        const options = {
+            method: "POST",
+            url: `${serverAddr}/chat/message`,
+            headers: { "Content-Type": "application/json" },
+            withCredentials: true,
+            data: data,
+        };
+        const response = await axios.request(options);
+        const newMessage = response.data.newMessage;
+        return newMessage;
+        /*
+        newMessage: {
+            chatRoomId: string;
+            createdAt: Date;
+            messageId: string;
+            senderId: string;
+            messageType: $Enums.MessageType;
+            isEdited: boolean;
+            isUnsent: boolean;
+            content: string;
+        }
+        */
+    } catch (error) {
+        throw error;
+    }
+};
+
+
+export const getChatRoomDetails = async (chatRoomId: string) => {
+    try {
+        const options = {
+            method: "GET",
+            url: `${serverAddr}/chat/${chatRoomId}/details`,
+            headers: { "Content-Type": "application/json" },
+            withCredentials: true,
+        };
+        const { data } = await axios.request(options);
+        const chatroom = data.chatRoom;
+        return chatroom;
+    /*
+    chatRoom: {
+        users: {
+            userId: string;
+            username: string;
+        }[];
+        messages: {
+            chatRoomId: string;
+            createdAt: Date;
+            messageId: string;
+            senderId: string;
+            messageType: $Enums.MessageType;
+            isEdited: boolean;
+            isUnsent: boolean;
+            content: string;
+        }[];
+    } & {
+        ...;
+    }
+    */
+    } catch (error) {
+        throw error;
+    }
+};
+
+
+export const addUserToChatRoom = async ({ chatRoomId, newUserId }: {
+    chatRoomId: string, newUserId: string
+}) => {
+    try {
+        const data = {
+            chatRoomId,
+            newUserId
+        };
+        const options = {
+            method: "POST",
+            url: `${serverAddr}/chat/addUser`,
+            headers: { "Content-Type": "application/json" },
+            withCredentials: true,
+            data: data,
+        };
+        const response = await axios.request(options);
+        const updatedChatRoom = response.data.updatedChatRoom;
+        return updatedChatRoom;
+        /*
+        updatedChatRoom: {
+            users: {
+                userId: string;
+                username: string;
+            }[];
+            messages: {
+                chatRoomId: string;
+                createdAt: Date;
+                messageId: string;
+                senderId: string;
+                messageType: $Enums.MessageType;
+                isEdited: boolean;
+                isUnsent: boolean;
+                content: string;
+            }[];
+        } & {
+            ...;
+        }
+        */
+    } catch (error) {
+        throw error;
+    }
+};
+
+
+export const unsendMessage = async (messageId: string) => {
+    try {
+        const options = {
+            method: "PUT",
+            url: `${serverAddr}/chat/message/${messageId}/unsend`,
+            headers: { "Content-Type": "application/json" },
+            withCredentials: true,
+        };
+        const response = await axios.request(options);
+        const updatedMessage = response.data.unsentMessage;
+        return updatedMessage;
+        /*
+        unsentMessage: {
+        chatRoomId: string;
+        createdAt: Date;
+        messageId: string;
+        senderId: string;
+        messageType: $Enums.MessageType;
+        isEdited: boolean;
+        isUnsent: boolean;
+        content: string;
+        }
+        */
+    } catch (error) {
+        throw error;
+    }
+};
+
+
+export const getAllGroupChatRooms = async () => {
+    try {
+        const options = {
+            method: "GET",
+            url: `${serverAddr}/chat/group-chatrooms`,
+            headers: { "Content-Type": "application/json" },
+            withCredentials: true,
+        };
+        const response = await axios.request(options);
+        const groupChatRooms = response.data.groupChatRooms;
+        return groupChatRooms;
+        /*
+        const groupChatRooms: {
+        chatRoomId: string;
+        groupName: string;
+        createdAt: Date;
+        users: {
+            userId: string;
+            username: string;
+            }[];
+        }[]
+        */
+    } catch (error) {
+        throw error;
+    }
+};
